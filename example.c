cfcdebc6fd6c14498285d804dd60bac038195c3d
Stefan Trawicki <s.trawicki@lancaster.ac.uk>
2020-08-27 14:57:45 +0100

    Comprehensive Refactor Testing card: https://www.notion.so/stefanuniversityworkspace/Comprehensive-Refactor-Testing-e4fa66173a964bf6a5cd065d843fafcb

--- example.c
+++ example.c
@@ -98,25 +98,25 @@ int run_tree(void *t) {
 
 int main(int argc, char **argv)
 {
-    int options = 4;
+    int options = 3;
     int l[options];
 
     behaviour_tree_t *tree = behaviour_tree_create("log.txt", IS_LOGGING);
     node_t *entry = node_create("entry", NULL, ENTRY, 0);
     behaviour_tree_set_root(tree, entry);
 
-    node_t *inverter = node_create("inverter", entry, INVERTER, 0);
+    node_t *i1 = node_create("inverter", entry, INVERTER, 0);
 
-    node_t *base_sequence = node_create("base fallback", inverter, SEQUENCE, 0);
+    node_t *s1 = node_create("s1", i1, SEQUENCE, 0);
 
-    node_t *fallback_1 = node_create("sequence 1", base_sequence, FALLBACK, 0);
-    node_t *sequence_1 = node_create("sequence 2", base_sequence, SEQUENCE, 0); 
+    node_t *leaf_1 = node_create("leaf1", s1, LEAF, 0);
+    node_t *s2 = node_create("s2", s1, FALLBACK, 0);
 
-    node_t *leaf_1 = node_create("leaf1", fallback_1, LEAF, 0);
-    node_t *leaf_2 = node_create("leaf2", fallback_1, LEAF, 0);
+    // node_t *s3 = node_create("s3", s1, SEQUENCE, 0);
 
-    node_t *leaf_3 = node_create("leaf3", sequence_1, LEAF, 0);
-    node_t *leaf_4 = node_create("leaf4", sequence_1, LEAF, 0);
+    node_t *leaf_2 = node_create("leaf2", s2, LEAF, 0);
+    node_t *i2 = node_create("inverter 2", s2, INVERTER, 0);
+    node_t *leaf_3 = node_create("leaf3", i2, LEAF, 0);
 
     uint8_t count = 0;
     int passed = 1;
@@ -127,14 +127,13 @@ int main(int argc, char **argv)
         leaf_configure(leaf_1, &Subject, test(l[0]));
         leaf_configure(leaf_2, &Subject, test(l[1]));
         leaf_configure(leaf_3, &Subject, test(l[2]));
-        leaf_configure(leaf_4, &Subject, test(l[3]));
 
-        int expected = !((l[0] || l[1]) && (l[2] && l[3]));
+        int expected = !( (l[0]) && (l[1] || !l[2]) );
         int actual = run_tree(tree);
 
         if (actual != expected) {
-            printf("Failed (exp %d w precon %d, %d, %d, %d, got %d)\n",
-           expected, l[0], l[1], l[2], l[3], actual);
+            printf("Failed (exp %d w precon %d, %d, %d, got %d)\n",
+           expected, l[0], l[1], l[2], actual);
            passed = 0;
         }
         count++;
