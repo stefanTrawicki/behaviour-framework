03c90d2fd82c6b09a0c7a408e7c9bb129da764a7
stefan <s.trawicki@lancaster.ac.uk>
2020-09-02 14:10:27 +0100

    Modules (#4)
    card: https://www.notion.so/stefanuniversityworkspace/Modules-90882c52d2854e29a6b32c990c811d0d

--- libs/behaviourtree.c
+++ libs/behaviourtree.c
@@ -1,4 +1,4 @@
-#include "tree.h"
+#include "behaviourtree.h"
 
 /* --------------------------------- Logging -------------------------------- */
 
@@ -8,48 +8,6 @@ void log_set_path(char *path)
     strcpy(log_path, path);
     CLEAR_LOG();
     return;
-}
-
-/* ---------------------------------- List ---------------------------------- */
-
-void list_create(List_t *list)
-{
-    list->count = 0;
-    list->index = -1;
-}
-
-void list_add(List_t *list, void *tar_to_add)
-{
-    list->count++;
-    list->arr = realloc(list->arr, sizeof(void *) * (list->count + 1));
-    list->arr[list->count - 1] = tar_to_add;
-}
-
-void *list_get(List_t *list, uint32_t index)
-{
-    return list->arr[index];
-}
-
-void *list_current(List_t *list)
-{
-    return list_get(list, list->index);
-}
-
-void *list_next(List_t *list)
-{
-    ASSERT_MSG(list->index == list->count, "List element out of range");
-    list->index++;
-    return list_current;
-}
-
-uint32_t list_is_next(List_t *list)
-{
-    return (list->index + 1) < (list->count);
-}
-
-uint32_t list_get_c(List_t *list)
-{
-    return list->count;
 }
 
 /* ---------------------------------- Nodes --------------------------------- */
